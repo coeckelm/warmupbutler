26cf27443699cb83a24a09d287ed98a3c1331d7f
Mario Coeckelberghs <mario.coeckelberghs@ing.be>
2018-01-22 15:03:09 +0100

    initial spring boot project

---
+++ warmupbutlerapi/src/main/java/net/ing/oc/fs1/warmupbutler/warmupbutlerapi/WarmupbutlerapiApplication.java
@@ -0,0 +1,12 @@
+package net.ing.oc.fs1.warmupbutler.warmupbutlerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmupbutlerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmupbutlerapiApplication.class, args);
+	}
+}
