e727272975f7f835ce7f70b46434a75df41e004f
Mario Coeckelberghs <mario.coeckelberghs@ing.be>
2018-02-05 08:10:17 +0100

    add getting started app content

--- src/main/java/net/ing/oc/fs1/warmupbutler/warmupbutlerapi/WarmupbutlerapiApplication.java
+++ src/main/java/net/ing/oc/fs1/warmupbutler/warmupbutlerapi/WarmupbutlerapiApplication.java
@@ -2,11 +2,17 @@ package net.ing.oc.fs1.warmupbutler.warmupbutlerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class WarmupbutlerapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WarmupbutlerapiApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
 	}
 }
