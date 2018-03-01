020f252327218cacd277d61067d63240547e18ce
Koen Van de Velde <koen.van.de.velde@ing.com>
2018-03-01 11:06:07 +0100

    updated test

--- src/main/test/net/ing/oc/fs1/warmupbutler/warmupbutler/EventResourceTest.java
+++ src/main/test/net/ing/oc/fs1/warmupbutler/warmupbutler/EventResourceTest.java
@@ -1,16 +1,38 @@
 package net.ing.oc.fs1.warmupbutler.warmupbutler;
 
+import net.ing.oc.fs1.warmupbutler.warmupbutler.model.Event;
 import net.ing.oc.fs1.warmupbutler.warmupbutler.resource.EventResource;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EventResourceTest {
+    @InjectMocks
+    private EventResource sut;
+    @Mock
+    private RedisTemplate redisTemplate;
 
     @Test
-    public void testApp() {
-        EventResource eventResource = new EventResource();
-        String result = eventResource.getEvent();
-        assertEquals( result, "Hello from Butler" );
+    public void getEventTest_happyFlow() {
+        Event event = new Event("", "first event");
+        ValueOperations valueOperations = Mockito.mock(ValueOperations.class);
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.get(any())).thenReturn(event);
+        String result = sut.getEvent("");
+        assertEquals( result, "Event found first event" );
     }
+
+
+
 
 }
